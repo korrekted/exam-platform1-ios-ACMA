fbbb3535a021d74cb2e86dccce5fc6648b11803f
akonst17 <akonst17@gmail.com>
2022-07-04 18:01:24 +0300

    copy

---
+++ ACMA/Classes/Misc/GlobalDefinitions.swift
@@ -0,0 +1,26 @@
+//
+//  GlobalDefinitions.swift
+//  Nursing
+//
+//  Created by Andrey Chernyshev on 16.01.2021.
+//
+
+final class GlobalDefinitions {
+    static let domainUrl = "https://exams2.mastrapi.com"
+    static let apiKey = "vCNncvW4FYcDPUFK"
+
+    static let sdkDomainUrl = "https://analytics.korrekted.com"
+    static let sdkApiKey = "7HmZW218jtXYujB0"
+
+    static let applicationTag = "EXAM_ACMA"
+
+    static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
+    static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+
+    static let appStoreUrl = "https://apps.apple.com/app/emt-prep/id1568136144" // TODO
+    static let appleAppID = "1626946259"
+
+    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
+    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
+    static let contactUsUrl = "https://quantresear.ch/contact.html"
+}
---
+++ ACMA/Resource/ACMA-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  ACMA-Bridging-Header.h
+//  ACMA
+//
+//  Created by Андрей Чернышев on 04.07.2022.
+//
+
+#ifndef ACMA_Bridging_Header_h
+#define ACMA_Bridging_Header_h
+
+#import <RushSDK/RushSDK.h>
+
+#endif /* ACMA_Bridging_Header_h */
