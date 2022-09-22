69c8a3c94d21dfa9cd4b2a460a2ccb4ca9220d02
akonst17 <akonst17@gmail.com>
2022-09-22 13:18:55 +0300

    интеграция sdk 2.0; новые урлы; build 4

--- ACMA/Classes/Misc/GlobalDefinitions.swift
+++ ACMA/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams2.mastrapi.com"
     static let apiKey = "vCNncvW4FYcDPUFK"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "7HmZW218jtXYujB0"
-
     static let applicationTag = "EXAM_ACMA"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "7HmZW218jtXYujB0"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "9v1eeGhHNyZ1M0fd"
 
     static let appStoreUrl = "https://apps.apple.com/app/mobility-associate-for-aruba/id1626946259"
     static let appleAppID = "1626946259"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
